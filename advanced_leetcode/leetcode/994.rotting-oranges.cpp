b23f902f4abda910e362a8156dec83497f2c4437
xiesiqi <coderxiesiqi1996@126.com>
2020-07-19 23:31:50 +0800

    leetcode

---
+++ advanced_leetcode/leetcode/994.rotting-oranges.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=994 lang=cpp
+ *
+ * [994] Rotting Oranges
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+        
+    }
+};
+// @lc code=end
+
